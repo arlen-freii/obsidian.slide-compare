6ba49264b6dc4a231d9a57d731a34fc191bf5ceb
Arlen Freii <arlen.freii@gmail.com>
2024-08-24 01:45:01 +0400

    feat: most basic functionality

--- main.ts
+++ main.ts
@@ -1,134 +1,136 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from "obsidian";
+import { Err, Ok, Result } from "ts-results";
 
-// Remember to rename these classes and interfaces!
+const BLOCK_DATA_PATTERN =
+	/^images: ?!\[\[(.+)\]\] ?!\[\[(.+)\]\] *(\r\n|\r|\n)*([\s\S]*)/g;
+const LANGUAGE_ALIAS = "compare-images";
+const IMAGE_FORMATS = [
+	"avif",
+	"bmp",
+	"gif",
+	"jpeg",
+	"jpg",
+	"png",
+	"svg",
+	"webp",
+];
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class SlideCompare extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerMarkdownCodeBlockProcessor(
+			LANGUAGE_ALIAS,
+			(source, el) => {
+				this.renderComparisonBlock(source, el);
+			},
+		);
 	}
 
-	onunload() {
+	renderComparisonBlock(source: string, el: HTMLElement) {
+		const blockData = this.parseBlockData(source);
 
+		const blockHTML = blockData.ok
+			? this.createComparisonBlock(blockData.val)
+			: this.createErrorBlock(blockData.val);
+		el.replaceWith(blockHTML);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	parseBlockData(
+		source: string,
+	): Result<{ images: Array<TFile>; figure: string }, string> {
+		const activeFile = this.app.workspace.getActiveFile()?.path;
+
+		// Figure out the best way to handle this.
+		// This should never happen in the first place.
+		if (activeFile === undefined) throw Error;
+
+		const blockDataMatch = [...source.matchAll(BLOCK_DATA_PATTERN)][0];
+		if (blockDataMatch === undefined) {
+			return Err("Invalid comparison block formatting.");
+		}
+
+		const leftImage = this.app.metadataCache.getFirstLinkpathDest(
+			blockDataMatch[1],
+			activeFile,
+		);
+		const rightImage = this.app.metadataCache.getFirstLinkpathDest(
+			blockDataMatch[2],
+			activeFile,
+		);
+		const figureText = blockDataMatch[4];
+
+		if (leftImage === null || rightImage === null) {
+			return Err("Invalid markdown embed file link.");
+		}
+		if (
+			!IMAGE_FORMATS.includes(leftImage.extension) ||
+			!IMAGE_FORMATS.includes(rightImage.extension)
+		) {
+			return Err("Markdown embed file provided isn't an image.");
+		}
+
+		return Ok({
+			images: [leftImage, rightImage],
+			figure: figureText,
+		});
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	createComparisonBlock(blockData: { images: Array<TFile>; figure: string }) {
+		const vault = blockData.images[0].vault;
+
+		const rootBlock = createDiv({ cls: "slide-compare" });
+
+		// Creating right first so it's below.
+		const rightImageContainer = rootBlock.createDiv({
+			cls: "sc-image-container sc-right-container",
+		});
+		const leftImageContainer = rootBlock.createDiv({
+			cls: "sc-image-container sc-left-container",
+		});
+
+		leftImageContainer.createEl("img", {
+			cls: "sc-compared-image sc-left-image",
+			attr: { src: vault.getResourcePath(blockData.images[0]) },
+		});
+		rightImageContainer.createEl("img", {
+			cls: "sc-compared-image sc-right-image",
+			attr: { src: vault.getResourcePath(blockData.images[1]) },
+		});
+
+		const sliderContainer = rootBlock.createDiv({
+			cls: "sc-slider-container",
+		});
+		sliderContainer.createDiv({ cls: "sc-ratio-slider" });
+
+		const listener = (e: MouseEvent) => {
+			const boundingBox = rootBlock.getBoundingClientRect();
+			const sliderRatio =
+				(e.clientX - boundingBox.left) /
+				(boundingBox.right - boundingBox.left);
+			rootBlock.style.setProperty("--sc-ratio", `${sliderRatio * 100}%`);
+		};
+
+		rootBlock.addEventListener("mousemove", (e) => {
+			if (e.buttons === 1) listener(e);
+		});
+		rootBlock.addEventListener("mousedown", (e) => {
+			listener(e);
+		});
+
+		return rootBlock;
+	}
+
+	createErrorBlock(message: string) {
+		const rootBlock = createDiv({ cls: "slide-compare" });
+		rootBlock.createEl("pre", {
+			cls: "sc-error-block",
+			text: ERROR_MESSAGE_WRAPPER.format(message),
+		});
+
+		return rootBlock;
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+const ERROR_MESSAGE_WRAPPER =
+	"Slide Compare has encountered an error:\n" +
+	"- {0}\n\n" +
+	"Please review this block and try again.";
