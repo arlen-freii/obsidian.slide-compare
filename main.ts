16a990959bd2270130cd81e802ccd829b134be13
Arlen Freii <arlen.freii@gmail.com>
2024-08-24 15:41:32 +0400

    style: return svg icon variable casing

--- main.ts
+++ main.ts
@@ -124,7 +124,7 @@ export default class SlideCompare extends Plugin {
 		const returnSliderButton = createDiv({
 			cls: "sc-return-slider-button edit-block-button",
 		});
-		returnSliderButton.innerHTML = returnSvg;
+		returnSliderButton.innerHTML = RETURN_SVG;
 		setTooltip(returnSliderButton, "Return slider");
 
 		returnSliderButton.addEventListener("click", () => {
@@ -151,5 +151,5 @@ const ERROR_MESSAGE_WRAPPER =
 	"- {0}\n\n" +
 	"Please review this block and try again.";
 
-const returnSvg =
+const RETURN_SVG =
 	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide lucide-undo-2"><path d="M9 14 4 9l5-5"/><path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5a5.5 5.5 0 0 1-5.5 5.5H11"/></svg>';
